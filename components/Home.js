a7237fb085c2f576bad00b5eac730f9e03821594
Nelson <nelson.guerreiro@nc-services.fr>
2023-07-16 17:37:18 +0200

    test

--- components/Home.js
+++ components/Home.js
@@ -127,7 +127,7 @@ body: JSON.stringify({ message: message, userId: user.id}),
         <div className="hidden xl:fixed xl:inset-y-0 xl:z-50 xl:flex xl:w-72 xl:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-black/10 px-6 ring-1 ring-white/5">
-            <div className="flex h-16 shrink-0 items-center">
+            <div className="flex h-16 shrink-0 items-center p-2 m-4">
               <img
                 className="h-22 w-auto transform -scale-x-100"
                 src="AdobeStock_607848635.png"
